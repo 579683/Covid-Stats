133ca28f74ee88221f31b0433d460b77f2b8a2b2
Christopher Nguyen <chrispy.nguyen@hotmail.com>
2021-03-30 15:14:00 +0200

    Added navigations and more sites

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./App.css";
+
+function Nav() {
+    return <nav>
+                <div>Logo</div>
+                <ul className="nav-links">
+                    <li>About</li>
+                </ul>
+            </nav>
+}
+
+export default Nav;
---
+++ src/pages/Home.js
@@ -0,0 +1,123 @@
+import React, {useEffect, useState} from 'react';
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import Columns from "react-columns";
+import Form from "react-bootstrap/Form";
+import NumberFormat from 'react-number-format';
+
+function Home() {
+
+    /* Relevant hooks */
+    const [latest, setLatest] = useState([]);
+    const [results, setResults] = useState([]);
+    const [searchCountries, setSearchCountries] = useState("");
+
+    /* Get the info from API's */
+    useEffect(() => {
+        axios
+        .all([
+            axios.get("https://disease.sh/v3/covid-19/all"),
+            axios.get("https://disease.sh/v3/covid-19/countries")
+        ])
+        .then(responseArr => {
+            setLatest(responseArr[0].data);
+            setResults(responseArr[1].data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }, []);
+
+    const date = new Date(parseInt(latest.updated));
+    const lastUpdated = date.toString();
+
+    const filterCountries = results.filter(item => {
+         return searchCountries !== "" ? item.country.toLowerCase().includes(searchCountries.toLowerCase()) : item;
+    })
+
+    const countries = filterCountries.map((data, i) => {
+        return (
+            <Card key={i} bg="light" text="dark" className="text-center" style={{margin: "10px"}}>
+                <Card.Img variant="top" src={data.countryInfo.flag} />
+                <Card.Body>
+                    <Card.Title>{data.country}</Card.Title>
+                    <Card.Text>Cases: {data.cases}</Card.Text>
+                    <Card.Text>Deaths: {data.deaths}</Card.Text>
+                    <Card.Text>Recovered: {data.recovered}</Card.Text>
+                    <Card.Text>Today's cases: {data.todayCases}</Card.Text>
+                    <Card.Text>Today's deaths: {data.todayDeaths}</Card.Text>
+                    <Card.Text>Active: {data.active}</Card.Text>
+                    <Card.Text>Critical: {data.critical}</Card.Text>
+                </Card.Body>
+            </Card>
+        );
+    });
+
+    var queries = [{
+        columns: 2,
+        query: 'min-width: 500px'
+    }, {
+        columns: 3,
+        query: 'min-width: 1000px'
+    }];
+
+    return (
+        <div>
+            <br />
+            <h2 style={{textAlign: "center"}}>Covid-19 Live Statistics</h2>
+            <br />
+            <CardDeck>
+
+            {/* Displays case count */}
+            <Card bg="secondary" text="white" className="text-center" style={{margin: "10px"}}>  
+                <Card.Body>
+                <Card.Title>Cases</Card.Title>
+                {/* <Card.Text>{latest.cases}</Card.Text> */}
+                <NumberFormat value={latest.cases} displayType={'text'} thousandSeparator={true} />
+                </Card.Body>
+                <Card.Footer>
+                <small>Last updated: {lastUpdated}</small>
+                </Card.Footer>
+            </Card>
+
+            {/* Displays death count */}
+            <Card bg="danger" text={"white"} className="text-center" style={{margin: "10px"}}>     
+                <Card.Body>
+                <Card.Title>Deaths</Card.Title>
+                {/* <Card.Text>{latest.deaths}</Card.Text> */}
+                <NumberFormat value={latest.deaths} displayType={'text'} thousandSeparator={true} />
+                </Card.Body>
+                <Card.Footer>
+                <small>Last updated: {lastUpdated}</small>
+                </Card.Footer>
+            </Card>
+
+            {/* Displays recovered count */}
+            <Card bg="success" text={"white"} className="text-center" style={{margin: "10px"}}>
+                <Card.Body>
+                <Card.Title>Recovered</Card.Title>
+                {/* <Card.Text>{latest.recovered}</Card.Text> */}
+                <NumberFormat value={latest.recovered} displayType={'text'} thousandSeparator={true} />
+                </Card.Body>
+                <Card.Footer>
+                <small>Last updated: {lastUpdated}</small>
+                </Card.Footer>
+            </Card>
+            </CardDeck>
+            <br />
+
+            {/* Displays search field */}
+            <Form>
+                <Form.Group controlId="formGroupSearch">
+                    <Form.Control type="text" placeholder="Search a country" onChange={(e => setSearchCountries(e.target.value))} />
+                </Form.Group>
+            </Form>
+
+            <Columns queries={queries}>{countries}</Columns>
+        </div>
+    );
+}
+
+export default Home;
